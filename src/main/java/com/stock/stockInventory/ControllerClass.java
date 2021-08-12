df56ab970f6e6c0b3c9040fd4c05f12697ee4680
Vandit Sharma <van0101@users.noreply.github.com>
2021-08-12 09:14:03 +0530

    Added exception handling

--- src/main/java/com/stock/stockInventory/ControllerClass.java
+++ src/main/java/com/stock/stockInventory/ControllerClass.java
@@ -1,12 +1,15 @@
 package com.stock.stockInventory;
 
 
+
+
 import com.stock.stockInventory.Stock.Stock;
 import com.stock.stockInventory.StockService.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,27 +23,58 @@ public class ControllerClass
     @GetMapping("/stock/display")
     public List<Stock> getStock()
     {
-        return this.stockService.getStock();
+        try
+        {
+            return this.stockService.getStock();
+        }
+        catch (Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Empty Inventory.",e);
+        }
     }
     // GET method to Display a Specific Stock
     @GetMapping("/stock/display/{stockId}")
     public Stock getStock(@PathVariable String stockId)
     {
-        return this.stockService.getStock(Long.parseLong(stockId));
+        try
+        {
+            return this.stockService.getStock(Long.parseLong(stockId));
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Stock is not in inventory.",e);
+        }
+
     }
 
     // POST method to create Stock
     @PostMapping("/stock/create")
     public Stock addStock(@RequestBody Stock stock)
     {
-        return this.stockService.addStock(stock);
+        try
+        {
+            return this.stockService.addStock(stock);
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Unable to add new entry.",e);
+        }
+
     }
 
     // Update stock from Inventory
     @PutMapping("/stock/update")
     public Stock updateStock(@RequestBody Stock stock)
     {
-        return this.stockService.updateStock(stock);
+        try
+        {
+            return this.stockService.updateStock(stock);
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Unable to update entry.",e);
+        }
+
     }
 
     // Delete Stock from Inventory
@@ -54,7 +88,7 @@ public class ControllerClass
         }
         catch(Exception e)
         {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Entry not found.");
         }
     }
 }
