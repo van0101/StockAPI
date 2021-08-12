e8651eb922d0cac5e37929a5def19d3e5cb3db2b
Vandit Sharma <van0101@users.noreply.github.com>
2021-08-12 06:51:53 +0530

    Initial Commit

---
+++ src/main/java/com/stock/stockInventory/StockInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInventoryApplication.class, args);
+	}
+
+}
